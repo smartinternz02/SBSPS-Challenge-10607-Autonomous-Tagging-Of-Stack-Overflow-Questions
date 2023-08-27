10afb6ff709d072dd714069fac4045acce1ee650
Shriyash Gulhane <106003535+shrigulhane100@users.noreply.github.com>
2023-08-27 14:23:23 +0530

    Update

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-# IMPORTING LIBRARIES
-import pandas as pd
-import numpy as np
-import ast
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import SGDClassifier, LogisticRegression
-from sklearn.svm import LinearSVC
-from sklearn.multiclass import OneVsRestClassifier
-import joblib
+import pickle
 
-# CONVERT Y COLUMN TO CLASSES
-multilabel = MultiLabelBinarizer()
-tfidf = TfidfVectorizer(analyzer='word', max_features=15, ngram_range=(1, 3), stop_words='english')
-sgd = SGDClassifier()
-svc = LinearSVC()
-labels = ['android', 'asp.net', 'c', 'c#', 'c++', 'css', 'html', 'ios', 'iphone',
-        'java', 'javascript', 'jquery', 'mysql', 'objective-c', 'php', 'python', 'ruby',
-        'ruby-on-rails', 'sql']
-
-# for classifier in [sgd, svc]:
-clf = OneVsRestClassifier(svc)
+with open('sklearn_objects.pkl', 'rb') as f:
+    mlb, p = pickle.load(f)
 
 
-# Load from file
-tagPredictorModel = joblib.load('tagPredictor.pkl')
-tfidfModel = joblib.load('tfidf.pkl')
+# predicting the model with samples
+classes = ['.net', 'android', 'asp.net', 'c', 'c#', 'c++', 'css', 'html', 'ios', 'iphone',
+ 'java', 'javascript', 'jquery', 'mysql', 'objective-c', 'php', 'python', 'ruby',
+ 'ruby-on-rails', 'sql']
 
-def getTags(question):
-    question = tfidfModel.transform(question)
-    tags = multilabel.inverse_transform(tagPredictorModel.predict(question))
-    print(tags)
+def suggestion(question):
+    x=[]
+    x.append(question)
+    mlb_predictions = p.predict([question])
+    tags = mlb.inverse_transform(mlb_predictions)    
+    suggested_tags = []
 
-ques = 'This is a good question about python and machine learning as well as pandas.'
-getTags([ques])
+    for tag in tags:
+        suggested_tags.append(tag)
+    
+    return suggested_tags
+
+
+def main():
+    # Get the question from the user
+    question = input("Enter your question: ")
+
+    # Predict the tags for the question
+    predicted_tags = suggestion(question)
+
+    # Print the predicted tags
+    print("Suggested tags:", predicted_tags)
+
+
+if __name__ == "__main__":
+    main()
